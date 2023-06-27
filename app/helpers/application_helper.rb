791feb3f2f98351fa4d8880cc0dad1b6dbb163b6
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-06-27 16:59:02 -0300

    refactor: refatoração dos arquivos.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def locale(locale)
-    (I18n.locale == "pt-BR") ? "Português do Brasil" : "Estados Unidos"
+  def locale
+    (I18n.locale == :en) ? "Estados Unidos" : "Português do Brasil"
   end
   def data_br(data_us)
     data_us.strftime("%d/%m/%Y")

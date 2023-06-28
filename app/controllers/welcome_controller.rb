30b2430ec597d6ca487ca3078497d1ce19676bde
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-06-28 17:21:42 -0300

    feat: Aprendendo sobre cookies e session.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    #session[:mensagem] = "Minha mensagem de teste! [session]"
+    #cookies[:mensagem] = "Minha mensagem de teste! [cookies]"
     @meu_nome = params[:nome]
   end
 end
